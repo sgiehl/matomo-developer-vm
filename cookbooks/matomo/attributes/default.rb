6891b70e3d6926f48442df12ebdc01b5e19dd4b4
sgiehl <stefan@matomo.org>
2021-07-19 18:00:18 +0200

    Ensure php directives are also inserted in php.ini for fpm

--- cookbooks/matomo/attributes/default.rb
+++ cookbooks/matomo/attributes/default.rb
@@ -10,4 +10,5 @@ default['redisio']['bin_path']        = '/usr/bin'
 default['redisio']['package_install'] = true
 default['redisio']['version']         = nil
 
+default['php']['fpm_ini_control'] = true
 default['php']['directives'] = { :'xdebug.max_nesting_level' => 200, :'memory_limit' => '512m', :'max_execution_time' => 90 }
